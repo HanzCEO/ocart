c40d9a428f672bce35f04d83036897bc519cf208
Hanz <hi@hanz.dev>
2024-12-21 05:25:34 +0700

    feat(backend): Added new declarations

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ CANISTER=$1
 echo Compiling for $CANISTER
 cargo build --release --target wasm32-unknown-unknown --package $CANISTER
 candid-extractor target/wasm32-unknown-unknown/release/$CANISTER.wasm > src/$CANISTER/$CANISTER.did
+dfx generate $CANISTER
--- src/backend/declarations/backend.did.js
+++ src/backend/declarations/backend.did.js
@@ -1,4 +1,49 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']) });
+  const Art = IDL.Record({
+    'name' : IDL.Text,
+    'description' : IDL.Text,
+    'image' : IDL.Text,
+  });
+  const DetectionAtom = IDL.Record({ 'art' : Art, 'confidence' : IDL.Nat32 });
+  const DetectionReport = IDL.Record({
+    'similarities' : IDL.Vec(DetectionAtom),
+  });
+  const Collection = IDL.Record({
+    'arts' : IDL.Vec(Art),
+    'name' : IDL.Text,
+    'description' : IDL.Text,
+  });
+  const Artist = IDL.Record({
+    'name' : IDL.Text,
+    'collections' : IDL.Vec(Collection),
+  });
+  return IDL.Service({
+    'get_detection_progress' : IDL.Func([IDL.Nat32], [IDL.Nat32], ['query']),
+    'get_detection_result' : IDL.Func(
+        [IDL.Nat32],
+        [DetectionReport],
+        ['query'],
+      ),
+    'get_inspiration_progress' : IDL.Func([IDL.Nat32], [IDL.Nat32], ['query']),
+    'get_inspiration_result' : IDL.Func([IDL.Nat32], [IDL.Text], ['query']),
+    'get_random_artists' : IDL.Func([IDL.Nat32], [IDL.Vec(Artist)], ['query']),
+    'get_random_arts' : IDL.Func([IDL.Nat32], [IDL.Vec(Art)], ['query']),
+    'get_random_collections' : IDL.Func(
+        [IDL.Nat32],
+        [IDL.Vec(Collection)],
+        ['query'],
+      ),
+    'update_detection_inference_session' : IDL.Func([], [IDL.Nat32], []),
+    'update_detection_session_data' : IDL.Func(
+        [IDL.Nat32, IDL.Text],
+        [IDL.Bool],
+        [],
+      ),
+    'update_inspiration_inference_session' : IDL.Func(
+        [IDL.Text],
+        [IDL.Nat32],
+        [],
+      ),
+  });
 };
 export const init = ({ IDL }) => { return []; };

cda9287a0bd3305d1c194d6f598064e47e5f7e74
root <root@blockchain>
2024-12-20 22:59:25 +0700

    feat(backend): Added backend functions

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,4 +1,68 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Deserialize};
+
+#[derive(CandidType, Deserialize)]
+struct Art {
+	name: String,
+	description: String,
+	image: String
 }
+
+#[derive(CandidType, Deserialize)]
+struct Collection {
+	name: String,
+	description: String,
+	arts: Art[]
+}
+
+#[derive(CandidType, Deserialize)]
+struct Artist {
+	name: String,
+	collections: Collection[]
+}
+
+#[derive(CandidType, Deserialize)]
+struct DetectionAtom {
+	art: Art,
+	confidence: u32
+}
+
+#[derive(CandidType, Deserialize)]
+struct DetectionReport {
+	similarities: DetectionAtom[]
+}
+
+///////////////////////////////////////////////////////////////////
+
+#[ic_cdk::query]
+fn get_random_artists(amount: u32) -> Artist[] {}
+
+#[ic_cdk::query]
+fn get_random_collections(amount: u32) -> Collection[] {}
+
+#[ic_cdk::query]
+fn get_random_arts(amount: u32) -> Art[] {}
+
+///////////////////////////////////////////////////////////////////
+
+#[ic_cdk::update]
+fn update_inspiration_inference_session(prompt: String) -> u32 {}
+
+#[ic_cdk::query]
+fn get_inspiration_progress(session_id: u32) -> u32 {}
+
+#[ic_cdk::query]
+fn get_inspiration_result(session_id: u32) -> String {}
+
+///////////////////////////////////////////////////////////////////
+
+#[ic_cdk::update]
+fn update_detection_inference_session() -> u32 {}
+
+#[ic_cdk::update]
+fn update_detection_session_data(id: u32, data: String) -> bool {}
+
+#[ic_cdk::query]
+fn get_detection_progress(id: u32) -> u32 {}
+
+#[ic_cdk::query]
+fn get_detection_result(id: u32) -> DetectionReport {}
